ba421d12394f0691d0579a1a6f30b935a6f587d7
Pureddy <pureddylavanya2002@gmail.com>
2023-03-24 22:32:13 +0530

    commit1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import *
+from flask import render_template,request,flash
 
 app=Flask(__name__)
 
@@ -11,9 +11,17 @@ def index():
 @app.route('/sub')
 def sub(methods=["GET","POST"]):
     if request.method=="POST":
-        return render_template('sub.html')
+        yr=request.form['year']
+        if yr>0:
+            if yr%4==0 and yr%100!=0 or yr%400==0:
+                return render_template('sub.html',result="Leap Year")
+            else:
+                return render_template('sub.html',result="Not a Leap Year")
+        else:
+            flash("Enter a valid year")
+            return render_template('sub.html')
     else:
         return render_template('index.html')
     
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5010)
