66a9ff08790ea0493881abda0942cbc58a17ae48
Pureddy <pureddylavanya2002@gmail.com>
2023-03-24 22:13:27 +0530

    updating source code

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/sub')
+def sub(methods=["GET","POST"]):
+    if request.method=="POST":
+        return render_template('sub.html')
+    else:
+        return render_template('index.html')
+    
 if __name__=='__main__':
     app.run(debug=True)
